ed24d3aa533ad635c1adde686e97d61456bbc6db
Gabriel <gabriel@nagelmichaelsen.de>
2022-04-02 15:38:27 +0200

    went back to guess_terminal

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -38,9 +38,10 @@
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
 
 mod = "mod4"
-terminal = "kitty" 
+terminal = guess_terminal() 
 
 keys = [
 

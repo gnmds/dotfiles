7d36d035ceab3be6656e2c22600b470dc74904a2
gabel <gabriel@nagelmichaelsen.de>
2022-05-09 20:54:09 +0200

    added alias and removed dot from zshrc

---
+++ config/zsh/alias.zsh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# shortcuts
+alias nv="nvim"
+alias c="clear"
+alias la="ls -a"
+alias ll="ls -la"
+
+# colors
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+
+# confirm file overwrite
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"

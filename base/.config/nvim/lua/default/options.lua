eec23d5fbbaf29f04318f9d4da3f7cf0600050b6
gabel <gabriel@nagelmichaelsen.de>
2022-05-20 20:08:16 +0200

    use a fancier method for setting options

--- base/.config/nvim/lua/default/options.lua
+++ base/.config/nvim/lua/default/options.lua
@@ -1,34 +1,42 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.backup = false
-vim.opt.clipboard = "unnamedplus"   -- default system clipboard
-vim.opt.cmdheight = 2               -- more cmd line space
-vim.opt.completeopt	= { "menuone", "noselect" }
-vim.opt.conceallevel = 0            -- displays text better
-vim.opt.fileencoding = "utf-8"
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.mouse = "a"
-vim.opt.pumheight = 10				  -- pop up menu height
-vim.opt.showtabline	= 2
-vim.opt.smartcase = true
-vim.opt.smartindent	= true
-vim.opt.showmode = false
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.swapfile = false
-vim.opt.timeoutlen = 1000			-- time to wait for a macro to complete in ms
-vim.opt.undofile = true
-vim.opt.updatetime = 300
-vim.opt.writebackup	= false
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop	= 4
-vim.opt.numberwidth	= 4
-vim.opt.wrap = false
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
+local options = {
+
+  number = true,
+  relativenumber = true,
+  backup = false,
+  clipboard = "unnamedplus",    -- default system clipboard
+  cmdheight = 2,                -- more cmd line space
+  completeopt	= { "menuone", "noselect" },
+  conceallevel = 0,             -- displays text better
+  fileencoding = "utf-8",
+  hlsearch = true,
+  ignorecase = true,
+  mouse = "a",
+  pumheight = 10,               -- pop up menu height
+  showtabline = 2,
+  smartcase = true,
+  smartindent = true,
+  showmode = false,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  timeoutlen = 1000,            -- time to wait for a macro to complete in ms
+  undofile = true,
+  updatetime = 300,
+  writebackup	= false,
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop	= 4,
+  numberwidth	= 4,
+  wrap = false,
+  scrolloff = 8,
+  sidescrolloff = 8
+
+}
 
 vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
 vim.cmd [[set iskeyword+=-]]

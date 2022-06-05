420616e7cd7a991e4b6ca56992474b0af16bbf4e
gabel <gabriel@nagelmichaelsen.de>
2022-06-05 10:47:54 +0200

    add treesitter

--- .config/nvim/lua/default/plugins.lua
+++ .config/nvim/lua/default/plugins.lua
@@ -67,6 +67,12 @@ return packer.startup(function(use)
     use 'nvim-telescope/telescope.nvim'
     use 'nvim-telescope/telescope-media-files.nvim'
 
+    -- treesitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
     if PACKER_BOOTSTRAP then
         require('packer').sync()
     end
